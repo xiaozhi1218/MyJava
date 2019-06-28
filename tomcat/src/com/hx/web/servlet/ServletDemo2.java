96cca501afbe0ac7508777dcfdb13ba0aea8cb5d
cyz <chenyongzhijjk@163.com>
2019-06-28 17:08:45 +0800

    servlet

---
+++ tomcat/src/com/hx/web/servlet/ServletDemo2.java
@@ -0,0 +1,35 @@
+package com.hx.web.servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @author chenyongzhi
+ * @create 2019/6/28 11:50
+ */
+public class ServletDemo2 implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
--- tomcat/web/index.jsp
+++ tomcat/web/index.jsp
@@ -1,8 +1,8 @@
 <%--
   Created by IntelliJ IDEA.
-  User: chenyongzhi
-  Date: 2019/6/27
-  Time: 18:42
+  User: hxzy
+  Date: 2019/6/28
+  Time: 8:54
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
